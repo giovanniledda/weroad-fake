f45c8f6cf4ba08eaa68043b8385dcef391f6c20b
Giovanni Ledda <ledda@netseven.it>
2023-04-15 07:50:02 +0200

    feat: fancy 404 error message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +49,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Exception|Throwable $exception)
+    {
+        if ($request->wantsJson()) {
+
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json(['message' => 'Entity not Found!'], 404);
+            }
+
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json(['message' => 'Route not Found!'], 404);
+            }
+        }
+
+
+        return parent::render($request, $exception);
     }
 }
