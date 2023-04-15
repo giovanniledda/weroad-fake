1306da81417c93b3cd4c966c8d69cf1b99452362
Giovanni Ledda <ledda@netseven.it>
2023-04-15 07:48:20 +0200

    feat: fancy abort message

--- app/Http/Middleware/EnsureUserHasRole.php
+++ app/Http/Middleware/EnsureUserHasRole.php
@@ -5,13 +5,16 @@ namespace App\Http\Middleware;
 use function abort;
 use Closure;
 use Illuminate\Http\Request;
+use function response;
 
 class EnsureUserHasRole
 {
     public function handle(Request $request, Closure $next, string $role): mixed
     {
         if (! $request->user()->hasRole($role)) {
-            abort(401);
+//            abort(401);
+            return response()
+                ->json(['message' => 'Unauthorized: action denied!'], 401);
         }
 
         return $next($request);
